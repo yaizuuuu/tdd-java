11c6ab6df493894d11a07c8ac320ca0e4ffaf228
yaizuuuu <p.i.o.y.m.a.a@gmail.com>
2018-11-02 00:34:10 +0900

    Chapter7 7章完了, こうしたらどうなるだろうという不安はテストに書き起こす

--- Tests/money/DollarTest.java
+++ Tests/money/DollarTest.java
@@ -30,6 +30,7 @@ class DollarTest {
         assertFalse(new Dollar(5).equals(new Dollar(6)));
         assertTrue(new Franc(5).equals(new Franc(5)));
         assertFalse(new Franc(5).equals(new Franc(6)));
+        assertFalse(new Dollar(5).equals(new Franc(5)));
     }
 
      @Test
