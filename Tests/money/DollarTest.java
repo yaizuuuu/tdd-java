52f292d0069dfad322038bd2204011dfffdb6202
yaizuuuu <p.i.o.y.m.a.a@gmail.com>
2018-11-01 23:59:00 +0900

    Chapter5 5章完了, 検証するためにはコピペをつかうなどあえて原則を破ってもよいが、最終的には正しく設計し、重複を削除する

--- Tests/money/DollarTest.java
+++ Tests/money/DollarTest.java
@@ -19,7 +19,7 @@ class DollarTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        
+
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
     }
@@ -29,4 +29,11 @@ class DollarTest {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
+
+     @Test
+    public void testFrancMultiplication() {
+         Franc five = new Franc(5);
+         assertEquals(new Franc(10), five.times(2));
+         assertEquals(new Franc(15), five.times(3));
+     }
 }
