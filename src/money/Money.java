9903c3f80f464c15e567b37dd99f1468bcbbe68e
yaizuuuu <p.i.o.y.m.a.a@gmail.com>
2018-11-02 23:28:59 +0900

    Chapter14 14章完了, 学習テスト=言語の仕様が想定通りかを確かめる, 実装時にテストがレッドになってしまった場合問題と思われる箇所を抽出してテストを書く, そしてそれをグリーンにしていく

--- src/money/Money.java
+++ src/money/Money.java
@@ -39,7 +39,8 @@ class Money implements Expression {
         return new Sum(this, addend);
     }
 
-    public Money reduce(String to) {
-        return this;
+    public Money reduce(Bank bank, String to) {
+        int rate = bank.rate(currency, to);
+        return new Money(amount / rate, to);
     }
 }
