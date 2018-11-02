459648c54c8cba60ae5bf65d931a77fc171642e1
yaizuuuu <p.i.o.y.m.a.a@gmail.com>
2018-11-02 21:24:16 +0900

    Chapter12 12章完了, 今後起きうる変更を考えて単一責任原則を守りクラスを細かくしていく, クラスを細かくしていく際にメタファー(=比喩, クラスの責務がイメージできる共通用語)を用いる

--- src/money/Money.java
+++ src/money/Money.java
@@ -1,6 +1,6 @@
 package money;
 
-class Money {
+class Money implements Expression {
     protected int amount;
     protected String currency;
 
@@ -33,5 +33,9 @@ class Money {
 
     String currency() {
         return currency;
+    }
+
+    Expression plus(Money addend) {
+        return new Money(amount + addend.amount, currency);
     }
 }
