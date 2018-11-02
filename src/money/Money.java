d06f393ef4abd975330f1f8a0549c648b68eeda3
yaizuuuu <p.i.o.y.m.a.a@gmail.com>
2018-11-02 20:31:29 +0900

    Chapter11 11章完了, リファクタリングを行い重複削除すると消えるクラスや消えるメソッドがでてくる, その過程でテストも消していく, 消せるものはどんどん消していく

--- src/money/Money.java
+++ src/money/Money.java
@@ -24,11 +24,11 @@ class Money {
     }
 
     static Money dollar(int amount) {
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
 
     static Money franc(int amount) {
-        return new Franc(amount, "CHF");
+        return new Money(amount, "CHF");
     }
 
     String currency() {
